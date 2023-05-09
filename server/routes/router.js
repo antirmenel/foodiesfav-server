a7892dfeb2aa9fd33ace0e4cb50ddc9f6df63577
Antir Menel <antirmenel@gmail.com>
2023-05-09 21:23:04 +0200

    figuring it out

--- server/routes/router.js
+++ server/routes/router.js
@@ -5,10 +5,17 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB database connection established successfully");
+  })
+  .catch((err) => {
+    console.log("Error connecting to the database: ", err);
+  });
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
