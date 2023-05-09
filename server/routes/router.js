63fcf445f03d64f834b4c93f0d36c270d246f62a
Antir Menel <antirmenel@gmail.com>
2023-05-10 00:52:33 +0200

    add stmp host name

--- server/routes/router.js
+++ server/routes/router.js
@@ -34,8 +34,13 @@ router.post("/register", async (req, res) => {
     }
 
     const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: { user: process.env.EMAIL, pass: process.env.PASSWORD },
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+      },
     });
 
     const mailOptions = {
