989f9156ea3a8313df17d6ad0d9d1f28692757e7
Antir Menel <antirmenel@gmail.com>
2023-05-10 12:15:31 +0200

    update

--- server/routes/router.js
+++ server/routes/router.js
@@ -17,13 +17,12 @@ mongoose
     console.log("Error connecting to the database: ", err);
   });
 
-// Create schema and model for email collection
 const emailSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
 });
+
 const Email = mongoose.model("Email", emailSchema);
 
-// Send mail and store email in database
 router.post("/register", async (req, res) => {
   const { email } = req.body;
 
@@ -43,9 +42,10 @@ router.post("/register", async (req, res) => {
       html: `<h1>Hi there!</h1><h4>Thank you for subscribing to our recipe newsletter! We are thrilled to have you on board and can't wait to share some of our favorite recipes with you.</h4><h4>We will be sending you a special recipe every month, so be sure to keep an eye out for our emails. <br/>If you have any questions or feedback, please don't hesitate to reach out to us at antirmenel@gmail.com.</h4><h3>Happy cooking!</h3>`,
     };
 
-    await sgMail.send(msg);
     const emailDoc = new Email({ email });
     await emailDoc.save();
+
+    await sgMail.send(msg);
 
     res.status(201).json({ status: 201 });
   } catch (error) {
